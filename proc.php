6b12ae65c39fb8d7e57c56d5181d30fadfe169c6
Krzysztof Chlistała <aureox@gmail.com>
2018-04-11 19:43:26 +0200

    Create proc.php

---
+++ proc.php
@@ -0,0 +1,34 @@
+<?php
+
+pcntl_async_signals(true);
+
+class Test
+{
+    private $exit = false;
+
+    public function doLoop()
+    {
+	while (true) {
+	    echo "loop\n";
+	    file_get_contents("http://ovh.net/files/1Gb.dat");
+            echo "xxx\n";
+            if ($this->exit) {
+                echo "bye\n";
+                exit;
+            }
+	}
+    }
+
+    public function sigHandler($sig)
+    {
+        $this->exit = true;
+    }
+}
+
+$test = new Test();
+
+pcntl_signal(SIGTERM, [$test, "sigHandler"]);
+pcntl_signal(SIGHUP,  [$test, "sigHandler"]);
+pcntl_signal(SIGUSR1, [$test, "sigHandler"]);
+
+$test->doLoop();
